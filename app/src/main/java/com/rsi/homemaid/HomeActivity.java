d7e9257a5a640ca6cac30b6b984377a7a52009dc
Deepak Sharma <deepak87.cs@gmail.com>
2017-07-17 13:35:43 +0530

    Configure BitBucket Successfully.

--- app/src/main/java/com/rsi/homemaid/HomeActivity.java
+++ app/src/main/java/com/rsi/homemaid/HomeActivity.java
@@ -7,9 +7,7 @@ import android.support.annotation.Nullable;
  * Created by deepak.sharma on 7/12/2017.
  */
 
-// First commit on bitbucket through IDE
 public class HomeActivity extends BaseActivity {
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
